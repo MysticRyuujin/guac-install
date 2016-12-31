5cd3dfa52fc184216e72a941b8b245df4fb99590
Chase Wright <mysticryuujin@gmail.com>
2016-12-31 16:40:40 -0600

    Updated to support Guacamole 0.9.10
    Latest versions as of December 31, 2016

--- guac-install.sh
+++ guac-install.sh
@@ -9,9 +9,7 @@ debconf-set-selections <<< "mysql-server mysql-server/root_password_again passwo
 read -s -p "Enter the password that will be used for the Guacamole database: " guacdbuserpassword
 
 # Install Features
-apt-get -y install libcairo2-dev libpng12-dev libossp-uuid-dev libfreerdp-dev libpango1.0-dev libssh2-1-dev libtelnet-dev libvncserver-dev libpulse-dev libssl-dev libvorbis-dev libwebp-dev mysql-server mysql-client mysql-common mysql-utilities tomcat8 freerdp ghostscript wget curl
-wget -O libjpeg-turbo-official_1.5.0_amd64.deb http://downloads.sourceforge.net/project/libjpeg-turbo/1.5.0/libjpeg-turbo-official_1.5.0_amd64.deb
-dpkg -i libjpeg-turbo-official_1.5.0_amd64.deb
+apt-get -y install libcairo2-dev libjpeg-turbo8-dev libpng12-dev libossp-uuid-dev libavcodec-dev libavutil-dev libswscale-dev libfreerdp-dev libpango1.0-dev libssh2-1-dev libtelnet-dev libvncserver-dev libpulse-dev libssl-dev libvorbis-dev libwebp-dev mysql-server mysql-client mysql-common mysql-utilities tomcat8 freerdp ghostscript jq
 
 # Add GUACAMOLE_HOME to Tomcat8 ENV
 echo "" >> /etc/default/tomcat8
@@ -19,15 +17,16 @@ echo "# GUACAMOLE EVN VARIABLE" >> /etc/default/tomcat8
 echo "GUACAMOLE_HOME=/etc/guacamole" >> /etc/default/tomcat8
 
 # Download Guacample Files
-wget -O guacamole-0.9.9.war http://downloads.sourceforge.net/project/guacamole/current/binary/guacamole-0.9.9.war
-wget -O guacamole-server-0.9.9.tar.gz http://sourceforge.net/projects/guacamole/files/current/source/guacamole-server-0.9.9.tar.gz
-wget -O guacamole-auth-jdbc-0.9.9.tar.gz http://sourceforge.net/projects/guacamole/files/current/extensions/guacamole-auth-jdbc-0.9.9.tar.gz
-wget -O mysql-connector-java-5.1.39.tar.gz http://dev.mysql.com/get/Downloads/Connector/j/mysql-connector-java-5.1.39.tar.gz
+SERVER=$(curl -s 'https://www.apache.org/dyn/closer.cgi?as_json=1' | jq --raw-output '.preferred')
+wget $SERVER/incubator/guacamole/0.9.10-incubating/source/guacamole-server-0.9.10-incubating.tar.gz
+wget $SERVER/incubator/guacamole/0.9.10-incubating/binary/guacamole-0.9.10-incubating.war
+wget $SERVER/incubator/guacamole/0.9.10-incubating/binary/guacamole-auth-jdbc-0.9.10-incubating.tar.gz
+wget https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.40.tar.gz
 
 #Extract Guacamole Files
-tar -xzf guacamole-server-0.9.9.tar.gz
-tar -xzf guacamole-auth-jdbc-0.9.9.tar.gz
-tar -xzf mysql-connector-java-5.1.39.tar.gz
+tar -xzf guacamole-server-0.9.10-incubating.tar.gz
+tar -xzf guacamole-auth-jdbc-0.9.10-incubating.tar.gz
+tar -xzf mysql-connector-java-5.1.40.tar.gz
 
 # MAKE DIRECTORIES
 mkdir /etc/guacamole
@@ -35,7 +34,7 @@ mkdir /etc/guacamole/lib
 mkdir /etc/guacamole/extensions
 
 # Install GUACD
-cd guacamole-server-0.9.9
+cd guacamole-server-0.9.10-incubating
 ./configure --with-init-dir=/etc/init.d
 make
 make install
@@ -44,11 +43,11 @@ systemctl enable guacd
 cd ..
 
 # Move files to correct locations
-mv guacamole-0.9.9.war /etc/guacamole/guacamole.war
+mv guacamole-0.9.10-incubating.war /etc/guacamole/guacamole.war
 ln -s /etc/guacamole/guacamole.war /var/lib/tomcat8/webapps/
 ln -s /usr/local/lib/freerdp/* /usr/lib/x86_64-linux-gnu/freerdp/.
-cp mysql-connector-java-5.1.39/mysql-connector-java-5.1.39-bin.jar /etc/guacamole/lib/
-cp guacamole-auth-jdbc-0.9.9/mysql/guacamole-auth-jdbc-mysql-0.9.9.jar /etc/guacamole/extensions/
+cp mysql-connector-java-5.1.40/mysql-connector-java-5.1.40-bin.jar /etc/guacamole/lib/
+cp guacamole-auth-jdbc-0.9.10-incubating/mysql/guacamole-auth-jdbc-mysql-0.9.10-incubating.jar /etc/guacamole/extensions/
 
 # Configure guacamole.properties
 echo "mysql-hostname: localhost" >> /etc/guacamole/guacamole.properties
@@ -65,13 +64,7 @@ service tomcat8 restart
 # Create guacamole_db and grant guacamole_user permissions to it
 echo "create database guacamole_db; create user 'guacamole_user'@'localhost' identified by \"$guacdbuserpassword\";GRANT SELECT,INSERT,UPDATE,DELETE ON guacamole_db.* TO 'guacamole_user'@'localhost';flush privileges;" | mysql -u root -p$mysqlrootpassword
 
-cat guacamole-auth-jdbc-0.9.9/mysql/schema/*.sql | mysql -u root -p$mysqlrootpassword guacamole_db
+cat guacamole-auth-jdbc-0.9.10-incubating/mysql/schema/*.sql | mysql -u root -p$mysqlrootpassword guacamole_db
 
-rm libjpeg-turbo-official_1.5.0_amd64.deb
-rm guacamole-server-0.9.9.tar.gz
-rm guacamole-auth-jdbc-0.9.9.tar.gz
-rm mysql-connector-java-5.1.39.tar.gz
-
-rm -rf mysql-connector-java-5.1.39/
-rm -rf guacamole-auth-jdbc-0.9.9/
-rm -rf guacamole-server-0.9.9/
+rm -rf guacamole-*
+rm -rf mysql-connector-java-5.1.40*
