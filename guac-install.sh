b00d8826a0fb07d3dc08dd970c1d166ab61a407d
Chase Wright <mysticryuujin@gmail.com>
2017-01-02 11:05:13 -0600

    Added wget & curl, just in case
    Some users have reported that wget and curl are sometimes not installed (weird)

--- guac-install.sh
+++ guac-install.sh
@@ -9,7 +9,7 @@ debconf-set-selections <<< "mysql-server mysql-server/root_password_again passwo
 read -s -p "Enter the password that will be used for the Guacamole database: " guacdbuserpassword
 
 # Install Features
-apt-get -y install libcairo2-dev libjpeg-turbo8-dev libpng12-dev libossp-uuid-dev libavcodec-dev libavutil-dev libswscale-dev libfreerdp-dev libpango1.0-dev libssh2-1-dev libtelnet-dev libvncserver-dev libpulse-dev libssl-dev libvorbis-dev libwebp-dev mysql-server mysql-client mysql-common mysql-utilities tomcat8 freerdp ghostscript jq
+apt-get -y install libcairo2-dev libjpeg-turbo8-dev libpng12-dev libossp-uuid-dev libavcodec-dev libavutil-dev libswscale-dev libfreerdp-dev libpango1.0-dev libssh2-1-dev libtelnet-dev libvncserver-dev libpulse-dev libssl-dev libvorbis-dev libwebp-dev mysql-server mysql-client mysql-common mysql-utilities tomcat8 freerdp ghostscript jq wget curl
 
 # Add GUACAMOLE_HOME to Tomcat8 ENV
 echo "" >> /etc/default/tomcat8
