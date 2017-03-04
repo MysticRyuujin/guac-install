e4198c279fa8caeafdeafe1fc5f5d093ab61acf1
Chase Wright <mysticryuujin@gmail.com>
2017-03-03 22:38:48 -0600

    Fixed $VERSION variable

--- docker-install.sh
+++ docker-install.sh
@@ -12,8 +12,8 @@ apt-get install docker.io mysql-client wget jq curl
 
 # Download the guacamole auth files for MySQL
 SERVER=$(curl -s 'https://www.apache.org/dyn/closer.cgi?as_json=1' | jq --raw-output '.preferred')
-wget $SERVER/incubator/guacamole/$VERSION-incubating/binary/guacamole-auth-jdbc-$VERSION-incubating.tar.gz
-tar -xzf guacamole-auth-jdbc-$VERSION-incubating.tar.gz
+wget $SERVER/incubator/guacamole/${VERSION}-incubating/binary/guacamole-auth-jdbc-${VERSION}-incubating.tar.gz
+tar -xzf guacamole-auth-jdbc-${VERSION}-incubating.tar.gz
 
 # Start MySQL
 docker run --restart=always --detach --name=mysql --env="MYSQL_ROOT_PASSWORD=$MYSQLROOTPASSWORD" --publish 3306:3306 mysql
@@ -30,9 +30,9 @@ flush privileges;"
 # Execute SQL Code
 echo $SQLCODE | mysql -h 127.0.0.1 -P 3306 -u root -p$MYSQLROOTPASSWORD
 
-cat guacamole-auth-jdbc-$VERSION-incubating/mysql/schema/*.sql | mysql -u root -p$MYSQLROOTPASSWORD -h 127.0.0.1 -P 3306 guacamole_db
+cat guacamole-auth-jdbc-${VERSION}-incubating/mysql/schema/*.sql | mysql -u root -p$MYSQLROOTPASSWORD -h 127.0.0.1 -P 3306 guacamole_db
 
 docker run --restart=always --name guacd -d glyptodon/guacd
 docker run --restart=always --name guacamole  --link mysql:mysql --link guacd:guacd -e MYSQL_HOSTNAME=127.0.0.1 -e MYSQL_DATABASE=guacamole_db -e MYSQL_USER=guacamole_user -e MYSQL_PASSWORD=$GUACDBUSERPASSWORD --detach -p 8080:8080 glyptodon/guacamole
 
-rm -rf guacamole-auth-jdbc-$VERSION-incubating*
+rm -rf guacamole-auth-jdbc-${VERSION}-incubating*
