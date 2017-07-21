911a18ab0c311741177c4585f78bc4218883cef4
Chase Wright <mysticryuujin@gmail.com>
2017-07-21 17:49:40 -0500

    Add files via upload

--- guac-upgrade.sh
+++ guac-upgrade.sh
@@ -1,4 +1,7 @@
+# Version Numbers of Guacamole and MySQL Connection/J to download
 VERSION="0.9.12"
+MCJVERSION = "5.1.43"
+
 SERVER=$(curl -s 'https://www.apache.org/dyn/closer.cgi?as_json=1' | jq --raw-output '.preferred|rtrimstr("/")')
 
 # Stop Tomcat
@@ -23,6 +26,12 @@ mv guacamole-${VERSION}-incubating.war /etc/guacamole/guacamole.war
 wget ${SERVER}/incubator/guacamole/${VERSION}-incubating/binary/guacamole-auth-jdbc-${VERSION}-incubating.tar.gz
 tar -xzf guacamole-auth-jdbc-${VERSION}-incubating.tar.gz
 cp guacamole-auth-jdbc-${VERSION}-incubating/mysql/guacamole-auth-jdbc-mysql-${VERSION}-incubating.jar /etc/guacamole/extensions/
+
+# Upgrade the MySQL Connector-J
+wget https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-${MCJVERSION}.tar.gz
+tar -xzf mysql-connector-java-${MCJVERSION}.tar.gz
+cp mysql-connector-java-${MCJVERSION}/mysql-connector-java-${MCJVERSION}-bin.jar /etc/guacamole/lib/
+rm -rf mysql-connector-java-${MCJVERSION}*
 
 # Check if there is an schema upgrade file, if there is run it (will prompt for password)
 if [ -f "guacamole-auth-jdbc-${VERSION}-incubating/mysql/schema/upgrade/upgrade-pre-${VERSION}.sql" ]
