24f39dbe1744fc7bccab26488f884f31f33b046e
Chase Wright <mysticryuujin@gmail.com>
2017-08-21 21:48:58 -0500

    Fixed Typos - Switched to apt

--- guac-install-server.sh
+++ guac-install-server.sh
@@ -11,21 +11,21 @@ else
 fi
 
 # Install Server Features
-apt-get update
-apt-get -qq -y install build-essential libcairo2-dev $JPEGTURBO libpng12-dev libossp-uuid-dev libavcodec-dev libavutil-dev \
+apt update
+apt -y install build-essential libcairo2-dev $JPEGTURBO libpng12-dev libossp-uuid-dev libavcodec-dev libavutil-dev \
 libswscale-dev libfreerdp-dev libpango1.0-dev libssh2-1-dev libtelnet-dev libvncserver-dev libpulse-dev libssl-dev \
 libvorbis-dev libwebp-dev jq curl wget
 
-# If Apt-Get fails to run completely the rest of this isn't going to work...
+# If apt fails to run completely the rest of this isn't going to work...
 if [ $? != 0 ]
 then
-    echo "apt-get failed to install all required dependencies."
+    echo "apt failed to install all required dependencies."
     exit
 fi
 
 SERVER=$(curl -s 'https://www.apache.org/dyn/closer.cgi?as_json=1' | jq --raw-output '.preferred|rtrimstr("/")')
 
-# Download Guacample Files
+# Download Guacamole Files
 wget ${SERVER}/incubator/guacamole/${VERSION}-incubating/source/guacamole-server-${VERSION}-incubating.tar.gz
 
 # Extract Guacamole Files
@@ -34,7 +34,7 @@ tar -xzf guacamole-server-${VERSION}-incubating.tar.gz
 # MAKE DIRECTORIES
 mkdir /etc/guacamole
 
-# Install GUACD
+# Install guacd
 cd guacamole-server-${VERSION}-incubating
 ./configure --with-init-dir=/etc/init.d
 make
