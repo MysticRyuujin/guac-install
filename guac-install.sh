4e5aea66e994eda5b1007874bc25a6c8b8a1c7a7
Chase Wright <mysticryuujin@gmail.com>
2018-01-20 19:31:42 -0500

    Update guac-install.sh

--- guac-install.sh
+++ guac-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Version numbers of Guacamole and MySQL Connector/J to download
-	
+GUACVERSION="0.9.14"
 MCJVERSION="5.1.45"
 
 # Update apt so we can search apt-cache for newest tomcat version supported
